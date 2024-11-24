66e985897394a70e8b3e41009853d52084bb9091
Husayn <habibullayevhusayn@gmail.com>
2024-11-24 20:14:19 +0500

    sd

---
+++ js/index.js
@@ -0,0 +1,11 @@
+const videos = document.querySelectorAll("video");
+
+videos.forEach((video) => {
+  video.addEventListener("play", () => {
+    videos.forEach((v) => {
+      if (v !== video) {
+        v.pause();
+      }
+    });
+  });
+});
